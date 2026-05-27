3b3f8fe5eb49454afed958536397ece1bcab691e
Ignacio Pelliza <ignape83@gmail.com>
2026-05-27 00:20:02 +0000

    Mi primer commit desde la tablet

---
+++ aa.py
@@ -0,0 +1,2 @@
+# Un código con un error de sintaxis obvio
+print("Esta línea nunca va a salir")
---
+++ parcial1.java
@@ -0,0 +1,5 @@
+public class parcial1 {
+    public static void main(String[] args) {
+        System.out.println("Probando parcial1");
+    }
+}
