bc995f1d01ed8da8b5c603f980c8bcaacfe1d8e2
Ignacio Pelliza <ignape83@gmail.com>
2026-05-29 21:47:16 +0000

    git

---
+++ ejercicio2_paradigma.java
@@ -0,0 +1,26 @@
+public class ejercicio2_paradigma {
+    public static class frecuente {
+      private Integer num;
+      private String dni;
+      private String nom;
+      private String apellido;
+      private Integer millas;
+      
+      public frecuente(Integer num, String dni, String nom, String apellido, Integer millas) {
+        this.num = num;
+        this.dni = dni;
+        this.nom = nom;
+        this.apellido = apellido;
+        this.millas = millas;
+      }
+      public Integer getMillas() {return millas;}
+      public Integer getNum() {return num;}
+      public String getDNI() {return dni;}
+      public String getNom() {return nom;}
+      public String getApellido() {return ;}
+      
+    } 
+    public static void main(String[] args) {
+        System.out.println("Probando ejercicio2_paradigma");
+    }
+}
